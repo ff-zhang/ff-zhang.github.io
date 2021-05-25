9a5577507b5725e7cc7a48f20c7dacd6bb4ab6f9
Felix Zhang <felixfzhang@hotmail.com>
2021-05-24 23:19:15 -0700

    Initial navbar setup

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="navbar">
+    <b-sidebar no-header shadow>
+      <nav class="mb-3">
+        <b-nav vertical>
+          <b-nav-item active>Active</b-nav-item>
+          <b-nav-item>link</b-nav-item>
+        </b-nav>
+      </nav>
+    </b-sidebar>
+  </div>
+</template>
+
+<script>
+// import { defineComponent } from '@vue/composition-api'
+
+export default ({
+  name: 'Navbar'
+  // setup() {
+    
+  // },
+})
+</script>
