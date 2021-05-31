b02660762b9d868ba411294737ae58fa4787fcc9
Felix Zhang <felixfzhang@hotmail.com>
2021-05-30 23:34:34 -0700

    refactor and added overlay

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,22 +1,45 @@
 <template>
-  <b-nav class="pt-3" vertical>
-    <b-nav-item @click="$emit('click', 'home')">Felix</b-nav-item>
-    <b-nav-item @click="$emit('click', 'education')">
-      <b-icon icon="journal-bookmark-fill"></b-icon>
-    </b-nav-item>
-    <b-nav-item @click="$emit('click', 'projects')">
-      <b-icon icon="hammer"></b-icon>
-    </b-nav-item>
-    <b-nav-item @click="$emit('click', 'goals')">
-      <b-icon icon="card-checklist"></b-icon>
-    </b-nav-item>
-  </b-nav>
+  <div class="sidenav">
+    <b-nav class="pt-3" justified vertical>
+      <b-nav-item class="mx-auto" @click="$emit('click', 'home')">Felix</b-nav-item>
+      <b-nav-item v-for="link in links" :key=link[0] class="mx-auto" @click="$emit('click', link[0])">
+        <b-icon :icon=link[1] font-scale="1.5"></b-icon>
+      </b-nav-item>
+      <!-- TODO: put settings icon at the bottom of the page -->
+      <b-nav-item class="mx-auto mt-5" @click="toggleOverlay">
+        <b-icon icon="gear" font-scale="1.5"></b-icon>
+      </b-nav-item>
+    </b-nav>
+    <b-overlay :show=show no-wrap>
+      <template #overlay>
+        <!-- TODO: add form for changing settings -->
+        <b-button ref="done" variant="outline-primary" size="sm" @click="toggleOverlay">
+          Done
+        </b-button>
+      </template>
+    </b-overlay>
+  </div>
 </template>
 
 <script>
-export default ({
-  name: 'Navbar'
-})
+export default {
+  name: 'Navbar',
+  methods: {
+    toggleOverlay() {
+      this.show = !this.show
+    }
+  },
+  data() {
+    return {
+      links: [
+        ['education', 'journal-bookmark-fill'],
+        ['projects', 'hammer'],
+        ['goals', 'card-checklist'],
+      ],
+      show: false
+    }
+  }
+}
 </script>
 
 <style scoped>
