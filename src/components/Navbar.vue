9476591643a73c1c70a5438a2ef7155381486380
Felix Zhang <felixfzhang@hotmail.com>
2021-05-30 18:52:48 -0700

    href to $emit on click

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,13 @@
 <template>
   <b-nav class="pt-3" vertical>
-    <b-nav-item href="#">Felix</b-nav-item>
-    <b-nav-item href="#">
+    <b-nav-item @click="$emit('click', 'home')">Felix</b-nav-item>
+    <b-nav-item @click="$emit('click', 'education')">
       <b-icon icon="journal-bookmark-fill"></b-icon>
     </b-nav-item>
-    <b-nav-item href="#">
+    <b-nav-item @click="$emit('click', 'projects')">
       <b-icon icon="hammer"></b-icon>
     </b-nav-item>
-    <b-nav-item href="#"> 
+    <b-nav-item @click="$emit('click', 'goals')">
       <b-icon icon="card-checklist"></b-icon>
     </b-nav-item>
   </b-nav>
